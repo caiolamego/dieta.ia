d0e78f3b866125e1d4124d872a9ac1946d706d6e
caiolamego <caio.lamego04@gmail.com>
2024-09-17 12:06:18 -0300

    integra api do gemini

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,12 +4,32 @@ import {
     FastifyRequest,
     FastifyReply
 } from "fastify"
+import { CreateNutritionController } from "./controllers/CreateNutritionController"
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
 
     fastify.get("/teste", (request: FastifyRequest, reply: FastifyReply) => {
-        console.log("ROTA CHAMADA!!")
+        let responseText = "```json\n{\n  \"nome\": \"Caio\",\n  \"sexo\": \"Masculino\",\n  \"idade\": 20,\n  \"altura\": 1.90,\n  \"peso\": 80,\n  \"objetivo\": \"Hipertrofia\",\n  \"refeicoes\": [\n    {\n      \"horario\": \"08:00\",\n      \"nome\": \"Café da Manhã\",\n      \"alimentos\": [\n        \"2 fatias de pão integral\",\n        \"2 ovos mexidos\",\n        \"1 banana\",\n        \"200ml de leite desnatado\"\n      ]\n    },\n    {\n      \"horario\": \"10:00\",\n      \"nome\": \"Lanche da Manhã\",\n        \"alimentos\": [\n        \"1 iogurte grego natural\",\n        \"1 scoop de whey protein\"\n      ]\n    },\n    {\n      \"horario\": \"12:00\",\n      \"nome\": \"Almoço\",\n      \"alimentos\": [\n        \"150g de frango grelhado\",\n        \"100g de arroz integral\",\n        \"100g de batata doce\",\n        \"Salada verde a vontade\"\n      ]\n    },\n    {\n      \"horario\": \"15:00\",\n      \"nome\": \"Lanche da Tarde\",\n        \"alimentos\": [\n        \"1 scoop de whey protein\",\n        \"2 fatias de pão integral\",\n        \"1 colher de sopa de pasta de amendoim\"\n      ]\n    },\n    {\n      \"horario\": \"19:00\",\n      \"nome\": \"Jantar\",\n      \"alimentos\": [\n        \"150g de carne vermelha magra\",\n        \"100g de batata doce\",\n        \"100g de brócolis\"\n      ]\n    },\n    {\n      \"horario\": \"21:00\",\n      \"nome\": \"Lanche da Noite\",\n        \"alimentos\": [\n        \"1 scoop de caseína\"\n      ]\n    }\n  ],\n  \"suplementos\": [\n    \"Whey Protein\",\n    \"Creatina\",\n    \"BCAA\"\n  ]\n}\n```"
+
+        try{
+            //Extrair JSON
+            let jsonString = responseText.replace(/```\w*\n/g, '').replace(/\n```/g, '').trim();
+
+            let jsonObject = JSON.parse(jsonString);
+
+            return reply.send({ data: jsonObject })
+
+
+        } catch(err){
+            console.log(err)
+        }
+
+
 
         reply.send({ ok: true})
+    })
+
+    fastify.post("/create", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateNutritionController().handle(request, reply)
     })
 }
